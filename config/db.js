f5c0f72c772c68b8e3b89481b10aa583f75b1f3e
Akshat <akshatpawar24by7@gmail.com>
2026-03-16 23:25:00 +0530

    update db config

--- config/db.js
+++ config/db.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-
-    await mongoose.connect("mongodb://127.0.0.1:27017/smartcampus");
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
-
   } catch (error) {
-
     console.error("MongoDB connection failed:", error);
     process.exit(1);
-
   }
 };
 
